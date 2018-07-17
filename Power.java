d7c5249ab37a92bd9bfaa8c633c3e37655d3592c
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-17 13:50:34 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,14 @@
+package janani;
+import java.util.Scanner;
+public class Power {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a,n,n1;
+Scanner ja=new Scanner(System.in);
+n=ja.nextInt();
+n1=ja.nextInt();
+a=(int)Math.pow(n,n1);
+System.out.println(a);
+}
+}
